ddcdc7622e37864f65e402bf2c062a46734644ad
faizanalibaig <riazfiaz6181@gmail.com>
2025-01-16 20:10:44 +0500

    Fetched data from API & Setup loading while data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,71 @@
+import { useEffect, useReducer, useState } from "react";
+
 import MoonToggleButton from './assets/moon-dark-theme.png';
-import SearchIcon from './assets/search.png';
+// import SearchIcon from './assets/search.png';
 import { IoSearchSharp } from 'react-icons/io5';
 
+
+const initialState = {
+    loading: true,
+    data: [],
+};
+
+const Reducer = (state, action) => {
+    switch (action.type) {
+        case "data":
+            return {
+                ...state,
+                data: action.payload,
+            };
+        case "load":
+            return {
+                ...state,
+                loading: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
 function App() {
+    const [state, dispatch] = useReducer(Reducer, initialState);
+
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const fetchedCountries = await fetch("https://restcountries.com/v3.1/all");
+                const results = await fetchedCountries.json();
+
+                dispatch({ type: "data", payload: results });
+                dispatch({ type: "load", payload: false });
+            } catch (e) {
+                console.error(e);
+            }
+        })();
+    }, [])
+
   return (
     <div
       style={{
-        height: '100vh',
+        height: 'auto',
         width: '100vw',
-        backgroundColor: '#000',
+        backgroundColor: '#202c37',
         display: 'flex',
         flexDirection: 'column',
+        overflow: 'hidden',
       }}
     >
       <Header />
       <Main />
-      <Countries />
+      <Countries countries={state?.data} loading={state?.loading}/>
     </div>
   );
 }
 
 function Header() {
   return (
-    <header className='flex justify-between bg-gray-800 text-white py-6 sm:py-8 px-6 sm:px-16 lg:px-20'>
+    <header className='flex justify-between bg-[#2b3945] text-white py-6 sm:py-8 px-6 sm:px-16 lg:px-20'>
       <h1 className='text-base sm:text-lg lg:text-xl font-bold font-pathway'>
         GlobeFinder
       </h1>
@@ -57,12 +100,12 @@ function Main() {
 
 function SearchByCountry() {
   return (
-    <div className='bg-gray-800 flex items-center gap-1 font-nunito w-full sm:w-[300px] lg:w-[400px] px-8 rounded-md'>
+    <div className='bg-[#2b3945] flex items-center gap-1 font-nunito w-full sm:w-[300px] lg:w-[400px] px-8 rounded-md'>
       <IoSearchSharp className='text-white text-xl' />
       <input
         type='text'
         placeholder='Search for a country'
-        className='bg-gray-800 py-3 px-2 w-96 rounded-r-md outline-none caret-white text-white'
+        className='bg-[#2b3945] py-3 px-2 w-96 rounded-r-md outline-none caret-white text-white'
       />
     </div>
   );
@@ -71,7 +114,7 @@ function SearchByCountry() {
 function FilterByRegion() {
   return (
     <div className='relative flex gap-3 font-nunito w-full sm:w-[220px] lg:w-[280px] rounded-md bg-gray-800'>
-      <select className='bg-gray-800 py-3 w-full px-3 rounded-md outline-none border-none text-white appearance-none'>
+      <select className='bg-[#2b3945] py-3 w-full px-3 rounded-md outline-none border-none text-white appearance-none'>
         <option value='' disabled selected className='hidden'>
           Filter By Region
         </option>
@@ -100,27 +143,33 @@ function FilterByRegion() {
   );
 }
 
-function Countries(){
+// eslint-disable-next-line react/prop-types
+function Countries({ countries, loading }){
     return(
-        <section
-            className="text-white px-6 sm:px-16 lg:px-20 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:gap-10 xl:gap-12">
-            <div className="bg-gray-800 h-96 sm:h-80 xl:h-96 rounded-md overflow-hidden">
-                <div className="h-[50%] bg-red-500">hello</div>
-                <div className={"p-5"}>second</div>
-            </div>
-            <div className="bg-gray-800 h-96 sm:h-80 xl:h-96 rounded-md overflow-hidden">
-                <div className="h-[50%] bg-red-500">hello</div>
-                <div className={"p-5"}>second</div>
-            </div>
-            <div className="bg-gray-800 h-96 sm:h-80 xl:h-96 rounded-md overflow-hidden">
-                <div className="h-[50%] bg-red-500">hello</div>
-                <div className={"p-5"}>second</div>
-            </div>
-            <div className="bg-gray-800 h-96 sm:h-80 xl:h-96 rounded-md overflow-hidden">
-                <div className="h-[50%] bg-red-500">hello</div>
-                <div className={"p-5"}>second</div>
-            </div>
-        </section>
+       <>
+           {
+               loading ? <h1>Loading</h1> : <section
+                   className="text-white pb-20 px-6 sm:px-16 lg:px-20 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:gap-10 xl:gap-12 overflow-hidden">
+                   {countries.map((country, index) => {
+                       return (
+                           <div key={index} className="bg-[#2b3945] h-96 sm:h-80 xl:h-96 rounded-md overflow-hidden">
+                               <div className="h-[50%]">
+                                   <img src={countries[index]?.flags?.png} alt={country.name}
+                                        className={"h-full w-full object-cover object-center"}/>
+                               </div>
+                               <div className={"p-6 font-pathway"}>
+                                   <h1 className="font-semibold pb-5">{countries[index]?.name?.common}</h1>
+                                   <h3>Population: {countries[index]?.population}</h3>
+                                   <h3>Region: {countries[index]?.region}</h3>
+                                   <h3>Capital: {countries[index]?.capital}</h3>
+                               </div>
+                           </div>
+                       )
+                   })}
+
+               </section>
+           }
+       </>
     )
 }
 
